21ad58405f89b88315d1a973590d2660ebffcac9
Michael Caldwell <michaelc@yingster.net>
2017-04-28 12:32:33 -0700

    Removed template file. Added functions to generate SVG for all the problems.

--- MathSheet.py
+++ MathSheet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import os, csv, platform, sys, random, datetime
+import os, csv, platform, sys, random, datetime, math
 
 # Globals
 operations = {"multiply" : "x",
@@ -10,15 +10,6 @@ operations = {"multiply" : "x",
 def error(msg):
     print("ERROR: %s" % msg)
     quit(1)
-
-def readFile(file):
-    inputBuffer = ""
-    with open(file, "rb") as fileIn:
-        fileIn.seek(0, 0)
-        for line in fileIn.readlines():
-            # Decaode text as LATIN for python 3 support
-            inputBuffer += line
-    return inputBuffer
 
 def writeSVGFile(outBuffer):
     tempFileName = "tmp.svg"
@@ -42,6 +33,177 @@ def createPDFFile(svgFile):
     import subprocess
     subprocess.call([inkscape, os.path.join(path, svgFile), "--export-pdf=%s" % os.path.join(path, pdfFile), "--without-gui"])
     return pdfFile
+
+def createTextBox(x, y, value, size, rightAlign=False):
+    align = "text-align:end;text-anchor:end;" if rightAlign == True else ""
+
+    textObject = """<text
+        xml:space="preserve"
+        style="font-style:normal;font-weight:normal;font-size:%spx;font-family:sans-serif;fill:#000000;fill-opacity:1;%s"
+        x="%s"
+        y="%s"><tspan
+            sodipodi:role="line"
+            x="%s"
+            y="%s"
+            style="font-size:%spx;%s">%s</tspan></text>
+""" % (size, align, x, y, x, y, size, align, value)
+    return textObject
+
+def generateSVGProblem(number, int1, int2, operation, x, y):
+    fontSize = 20
+    pnumber  = createTextBox(x,     y,    "%d." % number, fontSize)
+    topInt   = createTextBox(x+100, y+25, int1,      fontSize, True)
+    botInt   = createTextBox(x+100, y+50, int2,      fontSize, True)
+    operand  = createTextBox(x+25,  y+50, operation, fontSize)
+
+    line = """    <path
+       style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="m %s,%s 80,0"
+       id="path4162-8-0-5"
+       inkscape:connector-curvature="0" />
+""" % (x+20, y+55)
+
+    rectangle = """    <rect
+       style="opacity:1;fill:none;fill-opacity:1;stroke:#808080;stroke-width:1;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"
+       id="rect4168-2-5-1"
+       width="80"
+       height="30"
+       x="%s"
+       y="%s" />
+""" % (x+20, y+60)
+
+    problem = "%s\n%s\n%s\n%s\n%s\n%s\n" % (pnumber, topInt, botInt, operand, line, rectangle)
+    return problem
+
+def generateSVGAnswer(number, answer, x, y):
+    numberText = createTextBox(x + 40, y, "%d." % number, 20, True)
+    answerText = createTextBox(x + 50, y, answer, 20)
+    return "%s\n%s" % (numberText, answerText)
+
+def generateSVGDocument(date, content, answers):
+    document = """
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="8.5in"
+   height="11in"
+   viewBox="0 0 765 990"
+   id="svg2"
+   version="1.1"
+   inkscape:version="0.91 r13725"
+   sodipodi:docname="test.svg">
+  <defs
+     id="defs4" />
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="1.4"
+     inkscape:cx="315.41704"
+     inkscape:cy="258.71292"
+     inkscape:document-units="px"
+     inkscape:current-layer="layer1"
+     showgrid="false"
+     units="in"
+     inkscape:window-width="1920"
+     inkscape:window-height="1148"
+     inkscape:window-x="-8"
+     inkscape:window-y="-8"
+     inkscape:window-maximized="1"/>
+  <metadata
+     id="metadata7">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(0,-62.362205)">
+
+    <text
+      xml:space="preserve"
+      style="font-style:normal;font-weight:normal;font-size:40px;line-height:125%;font-family:sans-serif;fill:#000000;fill-opacity:1;"
+      x="84"
+      y="152">
+    <tspan
+      sodipodi:role="line"
+      x="84"
+      y="152"
+      style="font-size:17.5px">Date: """+date+"""</tspan>
+    </text>
+
+"""+content+"""
+"""+answers+"""
+
+  </g>
+</svg>"""
+
+    return document
+
+def generateSVGAnswerBlock(numberOfProblems, answers):
+    return """    <path
+      style="fill:none;fill-rule:evenodd;stroke:#808080;stroke-width:1;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:8, 8;stroke-dashoffset:0;stroke-opacity:1"
+      d="m -0.7,773 767,0"
+      id="path4401"
+      inkscape:connector-curvature="0" />
+
+    <text
+      xml:space="preserve"
+      style="font-style:normal;font-weight:normal;font-size:40px;line-height:125%;font-family:sans-serif;fill:#000000;fill-opacity:1"
+      x="85"
+      y="840">
+    <tspan
+      sodipodi:role="line"
+      id="tspan4283"
+      x="85"
+      y="840"
+      style="font-size:20px">Answers</tspan>
+    </text>
+
+"""+answers+"""
+
+    <rect
+      style="opacity:1;fill:none;fill-opacity:1;stroke:#808080;stroke-width:1;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"
+      width="155"
+      height="135"
+      x="540"
+      y="850" />
+
+    <text
+      xml:space="preserve"
+      style="font-style:normal;font-weight:normal;font-size:40px;font-family:sans-serif;fill:#000000;fill-opacity:1"
+      x="590"
+      y="960">
+    <tspan
+      sodipodi:role="line"
+      id="tspan4420"
+      x="590"
+      y="960">"""+str(numberOfProblems)+"""</tspan>
+    </text>
+
+    <path
+      style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+      d="m 567.8571,913.79078 101.42857,0"
+      id="path4422"
+      inkscape:connector-curvature="0" />"""
 
 def generateProblem(operation, limit1 = 20):
     if operation in ["multiply", "divide"]:
@@ -69,35 +231,33 @@ def generateProblem(operation, limit1 = 20):
 
 def main(argv):
     debug = False
+    numberOfProblems = 12
 
-    # Make sure template is available
-    file = "template.svg"
-    if not os.path.isfile(file):
-        error("Unable to locate template file, '%s'." % file)
-
-    # Read file in
-    buffer = readFile(file)
-
-    # Generate 12 problems, modify buffer
-    for i in range(1, 13):
+    # Generate SVG rendering of problems
+    questions = ""
+    answers = ""
+    for i in range(numberOfProblems):
         operation = operations.keys()[random.randint(0,len(operations)-1)]
         sign = operations[operation]
-
+        problem = i + 1
         int1, int2, answer = generateProblem(operation, 20)
 
         # Debug
         if debug == True:
             print("%d %s %d = %d" % (int1, sign, int2, answer))
 
-        output = ""
-        for line in buffer.split():
-            line = line.replace("$date", datetime.datetime.now().strftime("%m/%d/%y")) if "$date" in line else line
-            line = line.replace("$%dT" % i, str(int1)) if "$%dT" % i in line else line 
-            line = line.replace("$%dB" % i, str(int2)) if "$%dB" % i in line else line 
-            line = line.replace("$%dO" % i, sign) if "$%dO" % i in line else line
-            line = line.replace("$%dA" % i, str(answer)) if "$%dA" % i in line else line  
-            output += line+"\n"
-        buffer = output
+        x = (i % 3) * 230 + 80
+        y = math.floor(i / 3) * 150 + 190
+
+        questions += generateSVGProblem(problem, int1, int2, sign, x, y)
+
+        x = (i % 3) * 120 + 80
+        y = math.floor(i / 3) * 30 + 875
+
+        answers += generateSVGAnswer(problem, answer, x, y)
+
+    answerBlock = generateSVGAnswerBlock(numberOfProblems, answers)
+    buffer = generateSVGDocument(datetime.datetime.now().strftime("%m/%d/%y"), questions, answerBlock)
 
     # Write SVG File (tmp)
     svgFile = writeSVGFile(buffer)
